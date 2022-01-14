9bb9858929268634e77d19f3fe69b05585459042
Katie Klein <katieklein07@gmail.com>
2022-01-14 10:28:04 -0600

    Initial commit

--- src/main/java/youtunes/GenericDao.java
+++ src/main/java/youtunes/GenericDao.java
@@ -1,5 +1,22 @@
 package youtunes;
 
-public class GenericDao {
+import java.util.List;
 
+/*
+ * Generic interface for database manipulation
+ */
+
+
+public interface GenericDao<E, K> {
+	
+	public void add(E entity);
+	
+	public List<E> list();
+	
+	public E find(K key);
+	
+	public void update(E entity);
+	
+	public void remove(K key);
+	
 }
